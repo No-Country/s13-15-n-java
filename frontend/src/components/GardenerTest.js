961d88c997d85d7b3526470b2d5a37271b606146
ChrFont <roblesfontc@gmail.com>
2024-02-12 23:57:50 -0300

    initial proyect frontend

---
+++ frontend/src/components/GardenerTest.js
@@ -0,0 +1,20 @@
+'use client'
+
+import { decrement, increment, selectValue } from '../store/reducers/gardenerReducer'
+import { useSelector, useDispatch } from 'react-redux'
+export default function GardenerTest() {
+  const count = useSelector(selectValue)// useSelector para obtener el valor de nuestro estado cada vez que cambie
+  const dispatch = useDispatch() // useDispatch para ejecutar nuestros actions
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+
+      <h2> Gardener dispatch test {count}</h2>
+
+      <br />
+
+      <button onClick={() => dispatch(increment())}> Increment </button>
+      <br />
+      <button onClick={() => dispatch(decrement())}> Decrement </button>
+    </main>
+  );
+}
---
+++ frontend/src/store/reducers/gardenerReducer.js
@@ -0,0 +1,22 @@
+import { createSlice } from '@reduxjs/toolkit'
+
+const initialState = {
+  value: 0,
+}
+export const gardenerReducer = createSlice({
+  name: 'counter',
+  initialState,
+  reducers: {
+    increment: (state) => {
+      state.value += 1
+    },
+    decrement: (state) => {
+      state.value -= 1
+    }
+  },
+})
+
+export const { increment, decrement } = gardenerReducer.actions
+export const selectValue = (state) => state.counter.value;
+
+export default gardenerReducer.reducer
