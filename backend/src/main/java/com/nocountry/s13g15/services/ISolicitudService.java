acea8a43fab756b18f06435f0c9f70d145c0b321
Jarlin Fonseca <jarlinandres5000@gmail.com>
2024-02-23 15:52:56 -0500

    Se acomodo el endpoint de solicitar oferta

--- backend/src/main/java/com/nocountry/s13g15/services/ISolicitudService.java
+++ backend/src/main/java/com/nocountry/s13g15/services/ISolicitudService.java
@@ -1,10 +1,9 @@
 package com.nocountry.s13g15.services;
 
-import com.nocountry.s13g15.dto.request.SolicitudRequestDto;
 import com.nocountry.s13g15.dto.response.SolicitudResponseDto;
 
 public interface ISolicitudService {
 
-    public SolicitudResponseDto registrarSolicitud(SolicitudRequestDto solicitudRequestDto);
+     SolicitudResponseDto registrarSolicitud(Long ofertaId);
 
 }
