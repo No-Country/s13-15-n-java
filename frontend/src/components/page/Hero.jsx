ddadbbfa845e653520efa006ac8fc6545f57fb41
Dalianaguirre <danathap@gmail.com>
2024-02-26 00:55:41 -0500

    Agregando Landing Page

---
+++ frontend/src/components/page/Hero.jsx
@@ -0,0 +1,95 @@
+
+const Hero = () => {
+  return (
+    <div className="relative">
+      {/* Navbar */}
+      
+      <nav className="absolute top-0 left-0 right-0 flex items-center justify-between px-6 py-4 bg-transparent border-b border-gray-200 h-[104px]">
+        <div className="flex items-center lg:ml-16">
+          <h1 className="text-white font-montserrat" style={{fontSize: "38px"}}>GardenerGo</h1>
+        </div>
+        <div className="flex items-center lg:w-441">
+          <button className="text-white text-lg bg-transparent mx-2 hover:bg-white hover:text-black px-6 py-2 rounded-md transition duration-300 ease-in-out">Inicio</button>
+          <button className="text-white text-lg bg-transparent mx-2 hover:bg-white hover:text-black px-6 py-2 rounded-md transition duration-300 ease-in-out">Servicios</button>
+          <button className="text-white text-lg bg-transparent mx-2 hover:bg-white hover:text-black px-6 py-2 rounded-md transition duration-300 ease-in-out">Saber más</button>
+          <button className="text-white text-lg bg-transparent mx-2 hover:bg-white hover:text-black px-6 py-2 rounded-md transition duration-300 ease-in-out">Blog</button>
+        </div>
+        <div className="flex items-center space-x-12 lg:mr-16">
+          <button className="bg-white text-[#073A38] hover:bg-[#53BC65] hover:text-white px-6 py-3 rounded-md transition duration-300 ease-in-out">Iniciar Sesión</button>
+          <button className="bg-white text-[#073A38] hover:bg-[#53BC65] hover:text-white px-6 py-3 rounded-md transition duration-300 ease-in-out">Registrarse</button>
+        </div>
+      </nav>
+      
+      {/* Hero content */}
+      <div className="bg-cover bg-center w-full h-[1024px]" style={{backgroundImage: "url('/assets/bg-heroo.png')"}}>
+        <div className="flex flex-col items-center h-full">
+          <h1 className="text-white text-[128px] lg:mx-100 mt-44 text-center font-semibold leading-[128px]">El jardín <br></br> como arte</h1>
+          <div className="relative md:ml-52 md:mr-52 mt-12 text-3xl leading-9 text-center text-white max-md:max-w-full">
+            Aprovechá el mantenimiento de jardines, el paisajismo, la cirugía de
+            árboles y otros servicios más, brindados por profesionales capacitados
+            y altamente experimentados.
+          </div>
+          <div className="flex space-x-12 mt-16">
+            <button className="bg-white w-[238px] h-[80px] text-[#073A38] text-xl hover:bg-[#073A38] hover:text-white px-6 py-3 rounded-md transition duration-300 ease-in-out">Contacto</button>
+            <button className="bg-white w-[238px] h-[80px] text-[#073A38] text-xl hover:bg-[#073A38] hover:text-white px-6 py-3 rounded-md transition duration-300 ease-in-out">Saber más</button>
+          </div>
+
+          {/* Card final */}
+          <div className="hidden lg:flex relative gap-5 justify-between px-12 py-6 mt-14 mb-8 w-full text-center whitespace-nowrap bg-white rounded-xl max-w-[1278px] max-md:flex-wrap max-md:px-5 max-md:mt-10 max-md:max-w-full">
+            <div className="flex flex-col flex-1">
+              <div className="self-center text-5xl font-semibold text-green-400 leading-[61.2px] max-md:text-4xl">
+                  <span className="text-stone-900">5</span>
+                  <span className="text-[#53BC65]">+</span>
+              </div>
+              <div className="mt-3 text-base font-medium leading-5 text-stone-900">
+                  Años en el mercado
+              </div>
+            </div>
+            <div className="flex flex-col flex-1 my-auto">
+              <div className="self-center text-5xl font-semibold text-green-400 leading-[61.2px] max-md:text-4xl">
+                  <span className="text-stone-900">1.5m</span>
+                  <span className="text-[#53BC65]">+</span>
+              </div>
+              <div className="mt-3 text-base font-medium leading-5 text-stone-900">
+                  Clientes por todo el mundo
+              </div>
+            </div>
+            <div className="flex flex-col flex-1">
+            <div className="self-center text-5xl font-semibold text-green-400 leading-[61.2px] max-md:text-4xl">
+                <span className="text-stone-900">20</span>
+                <span className="text-[#53BC65]">+</span>
+            </div>
+            <div className="mt-3 text-base font-medium leading-5 text-stone-900">
+                Trabajos finalizados
+            </div>
+            </div>
+            <div className="flex flex-col flex-1">
+            <div className="text-5xl font-semibold text-green-400 leading-[61.2px] max-md:text-4xl">
+                <span className="text-stone-900">30m</span>
+                <span className="text-[#53BC65]">+</span>
+            </div>
+            <div className="self-center mt-3 text-base font-medium leading-5 text-stone-900">
+                Jardineros
+            </div>
+            </div>
+            <div className="flex flex-col flex-1">
+            <div className="text-5xl font-semibold text-green-400 leading-[61.2px] max-md:text-4xl">
+                <span className="text-stone-900">7m</span>
+                <span className="text-[#53BC65]">+</span>
+            </div>
+            <div className="mt-3 text-base font-medium leading-5 text-stone-900">
+                Árboles plantados
+            </div>
+            </div>
+        </div>
+
+
+        </div>
+        
+      </div>      
+    </div>
+  );
+};
+
+export default Hero;
+
---
+++ frontend/src/components/page/OurServices.jsx
@@ -0,0 +1,142 @@
+import React from 'react'
+
+const OurServices = () => {
+  return (
+    <div className="flex flex-col px-5 mt-24">
+      <div className="self-center text-7xl font-semibold text-center leading-[70.35px] text-cyan-950 max-md:max-w-full max-md:text-4xl">
+        <span className="text-cyan-950">Nuestros</span>
+        <span className="text-cyan-950"> Servicios</span>
+      </div>
+      <div className="self-center mt-8 text-base font-medium leading-5 text-center text-stone-900 max-md:max-w-full">
+      Vea la lista detallada de los servicios disponibles que brindamos
+      </div>
+      <div className="mt-20 w-full max-md:mt-10 max-md:max-w-full">
+        <div className="flex gap-5 max-md:flex-col max-md:gap-0 max-md:">
+          <div className="flex flex-col w-[33%] max-md:ml-0 max-md:w-full">
+            <div className="flex flex-col justify-center px-6 py-10 mx-auto w-full font-semibold bg-white rounded-2xl shadow-lg leading-[120%] max-md:px-5 max-md:mt-6">
+                <div className="flex gap-5 justify-between text-3xl text-[#53BC65] whitespace-nowrap">
+                <img
+                    loading="lazy"
+                    src="https://cdn.builder.io/api/v1/image/assets/TEMP/b446b1ccfa30791b029f633dc0f5361434b3027e40a200e8c8a919bc062f48aa?"
+                    className="w-16 aspect-square fill-white"
+                />
+                <div className="grow my-auto">Corte de césped</div>
+                </div>
+                <div className="mt-6 text-base font-medium leading-6 text-stone-900">
+                Mantenga su césped en óptimas condiciones sin mover un dedo y
+                mejore la apariencia de su propiedad.
+                </div>
+                <div className="justify-center self-center px-10 py-4 mt-6 text-xl text-center text-white whitespace-nowrap rounded-xl bg-[#073a38] max-md:px-5">
+                Más info
+                </div>
+            </div>
+          </div>
+          <div className="flex flex-col ml-5 w-[33%] max-md:ml-0 max-md:w-full">
+            <div className="flex flex-col grow justify-center px-6 py-10 mx-auto w-full font-semibold bg-white rounded-2xl shadow-lg max-md:px-5 max-md:mt-6">
+                <div className="flex gap-5 justify-between text-3xl leading-9 text-[#53BC65]">
+                <img
+                    loading="lazy"
+                    src="https://cdn.builder.io/api/v1/image/assets/TEMP/fb409ea34e4adcc96df5509aada8cab2b5d08b4628f44a480b44b3b568109aa9?"
+                    className="my-auto w-16 aspect-square fill-white"
+                />
+                <div className="flex-auto">Mantenimiento de Jardines</div>
+                </div>
+                <div className="mt-6 text-base font-medium leading-6 text-stone-900">
+                Mantenga su espacio verde ordenado en todo momento. Contrate a
+                un profesional para que sus plantas luzcan presentables.
+                </div>
+                <div className="justify-center self-center px-10 py-4 mt-6 text-xl leading-6 text-white whitespace-nowrap rounded-xl bg-[#073a38] max-md:px-5">
+                Más info
+                </div>
+            </div>
+          </div>
+          <div className="flex flex-col ml-5 w-[33%] max-md:ml-0 max-md:w-full">
+            <div className="flex flex-col justify-center px-6 py-10 mx-auto w-full font-semibold bg-white rounded-2xl shadow-lg max-md:px-5 max-md:mt-6">
+                <div className="flex gap-5 justify-between text-3xl leading-8 text-[#53BC65]">
+                <img
+                    loading="lazy"
+                    src="https://cdn.builder.io/api/v1/image/assets/TEMP/f2561bec5817e388d825824a6527d34ed6944413bc85ea7546a8b46ab0d11761?"
+                    className="w-16 aspect-square fill-white"
+                />
+                <div className="flex-auto">Diseño de paisajismo y jardín</div>
+                </div>
+                <div className="mt-6 text-base font-medium leading-6 text-stone-900">
+                Convertí el jardín de tus sueños en realidad con los paisajistas
+                experimentados que pueden ejecutar cualquier tipo de renovación.
+                </div>
+                <div className="justify-center self-center px-10 py-4 mt-6 text-xl leading-6 text-white whitespace-nowrap rounded-xl bg-[#073a38] max-md:px-5">
+                Más info
+                </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="mt-6 w-full max-md:max-w-full">
+        <div className="flex gap-5 max-md:flex-col max-md:gap-0 max-md:">
+            <div className="flex flex-col w-[33%] max-md:ml-0 max-md:w-full">
+            <div className="flex flex-col justify-center px-6 py-10 mx-auto w-full font-semibold bg-white rounded-2xl shadow-lg leading-[120%] max-md:px-5 max-md:mt-6">
+                <div className="flex gap-5 justify-between text-3xl text-[#53BC65] whitespace-nowrap">
+                <img
+                    loading="lazy"
+                    src="https://cdn.builder.io/api/v1/image/assets/TEMP/c5a2b4e4c97a137df1a9b93940743553a3e735b8cba6488d0c30d718f596ac59?"
+                    className="w-16 aspect-square fill-white"
+                />
+                <div className="grow my-auto">Poda de árboles</div>
+                </div>
+                <div className="mt-6 text-base font-medium leading-6 text-stone-900">
+                Poda de árboles, modelado de copas, desbastado de tocones y más
+                operaciones, ejecutadas por profesionales capacitados y
+                altamente calificados
+                </div>
+                <div className="justify-center self-center px-10 py-4 mt-6 text-xl text-white whitespace-nowrap rounded-xl bg-[#073a38] max-md:px-5">
+                Más info
+                </div>
+            </div>
+            </div>
+            <div className="flex flex-col ml-5 w-[33%] max-md:ml-0 max-md:w-full">
+            <div className="flex flex-col grow justify-center px-6 py-10 mx-auto w-full font-semibold bg-white rounded-2xl shadow-lg max-md:px-5 max-md:mt-6">
+                <div className="flex gap-5 justify-between text-3xl leading-9 text-[#53BC65]">
+                <img
+                    loading="lazy"
+                    src="https://cdn.builder.io/api/v1/image/assets/TEMP/0c52c607f2e560d889b173e9dfa16032a722dfab0e821cf3f63b84ff021a4ac4?"
+                    className="my-auto w-16 aspect-square fill-white"
+                />
+                <div className="flex-auto">Instalación de valla</div>
+                </div>
+                <div className="mt-6 text-base font-medium leading-6 text-stone-900">
+                Proporcione privacidad de los vecinos entrometidos o brinde
+                seguridad adicional para su familia y sus mascotas instalando
+                cualquier tipo de cercas.
+                </div>
+                <div className="justify-center self-center px-10 py-4 mt-6 text-xl leading-6 text-white whitespace-nowrap rounded-xl bg-[#073a38] max-md:px-5">
+                Más info
+                </div>
+            </div>
+            </div>
+            <div className="flex flex-col ml-5 w-[33%] max-md:ml-0 max-md:w-full">
+            <div className="flex flex-col grow justify-center px-6 py-10 mx-auto w-full font-semibold bg-white rounded-2xl shadow-lg max-md:px-5 max-md:mt-6">
+                <div className="flex gap-5 justify-between text-3xl leading-9 text-[#53BC65]">
+                <img
+                    loading="lazy"
+                    src="https://cdn.builder.io/api/v1/image/assets/TEMP/ecd851444118f2ec1090cdda7c4fe13cb2369c37f47b22f69caa97cf1e7a3cc8?"
+                    className="my-auto w-16 aspect-square fill-white"
+                />
+                <div className="flex-auto">Cuidado de las plantas</div>
+                </div>
+                <div className="mt-6 text-base font-medium leading-6 text-stone-900">
+                Cuida cada pequeña planta que tengas en tu sitio, incluidas
+                flores, arbustos y árboles pequeños. El riego y mantenimiento es
+                importante también para ellos.
+                </div>
+                <div className="justify-center self-center px-10 py-4 mt-6 text-xl leading-6 text-white whitespace-nowrap rounded-xl bg-[#073a38] max-md:px-5">
+                Más info
+                </div>
+            </div>
+            </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default OurServices
