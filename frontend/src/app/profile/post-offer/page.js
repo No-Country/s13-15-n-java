2c23f4d1620940c3725e14e460b34bc434fc48bb
ChrFont <roblesfontc@gmail.com>
2024-02-27 23:51:58 -0300

    add html

--- frontend/src/app/profile/post-offer/page.js
+++ frontend/src/app/profile/post-offer/page.js
@@ -3,7 +3,16 @@ export default function PostOffer() {
   return (
 
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <h1>Publicar postulaciones</h1>
+      <h1>Publicar oferta</h1>
+      <form>
+        <label>Titulo<input></input></label>
+        <label>Descripcion de la oferta<input></input></label>
+        <label>Complejidad<input></input></label>
+        <label>Fecha inicio<input></input></label>
+        <label>Fecha expiracion<input></input></label>
+        <label>Pago hasta:<input></input></label>
+        <button>Publicar oferta</button>
+      </form>
     </main>
 
   );
